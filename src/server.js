d6dee12f5e757d3c025451503476b3785e03a8b9
Joatan Carlos <joatancarlos@gmail.com>
2022-10-28 11:37:08 -0300

    adicionando libs express e mongoose

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+
+const app = express()
+
+// definindo o template engine
+app.set('view engine', 'ejs')
+
+// definindo os arquivos públicos
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Habilita server para receber dados via post (formulário)
+app.use(express.urlencoded({ extended: true}))
+
+// rotas
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home'
+    })
+})
+
+// 404 error (not found)
+app.use((req, res) => {
+    res.send('Page not found')
+})
+
+// executando o servidor
+const port = process.env.PORT || 8080
+app.listen(port, () =>{console.log(`Server in port ${port}`)});
