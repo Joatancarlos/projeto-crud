fb087a01352d773bbc7315cec22cf3b18f13c123
Joatan Carlos <joatancarlos@gmail.com>
2022-11-03 20:37:19 -0300

    adicionando feature de cadastro, edicao, listagem e remocao de clientes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
+
+
+const db = require('./database')
+const routes = require('./routes')
 
 const app = express()
+
+// conexão com o banco de dados
+db.connect()
+
+// Criando tabela no mongo com os campos name, age, email e password tendo o nome de 'customers'
+
+
 
 // definindo o template engine
 app.set('view engine', 'ejs')
@@ -15,12 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Habilita server para receber dados via post (formulário)
 app.use(express.urlencoded({ extended: true}))
 
-// rotas
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Home'
-    })
-})
+// definindo as rotas
+
+app.use('/', routes)
 
 // 404 error (not found)
 app.use((req, res) => {
